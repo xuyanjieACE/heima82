4f5029b431ad9b1d16cb89d3c0edebd872df8a56
xyj <6666666@139.com>
2019-06-02 22:31:59 +0800

    修改后

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("BĞŞ¸ÄµÄ´úÂë");
+		int i =1;
+		System.out .println(i+2);
     }
 }
